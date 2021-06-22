1b2b00aa7501a54887cd8317adadd18235597a5d
Ольга <tekuschaya@MacBook.local>
2021-06-22 19:49:03 +0300

    v2

--- main.go
+++ main.go
@@ -1,17 +1,47 @@
 package main
 
 import (
-	"bufio"
+	//"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	str := readInput()
-	fmt.Println(len(str))
+	args := os.Args[1:]
+	//a := Scan1()
+	//fmt.Println(args[0])
+	if len(args) > 0 {
+		arr := strings.Split(args[0], " ")
+		//fmt.Println(arr)
+		fmt.Println(len(arr))
+	} else {
+		fmt.Println('0')
+	}
+	//fmt.Println("LINE1 : ", Scan1())
+	//fmt.Println("LINE2 : ", Scan2())
+	//str := readInput()
+	//fmt.Println(len(str))
 }
+/*func Scan1() string {
+	in := bufio.NewScanner(os.Stdin)
+	in.Scan()
+	if err := in.Err(); err != nil {
+	  fmt.Fprintln(os.Stderr, "Ошибка ввода:", err)
+	}
+	return in.Text()
+  }*/
+  
+  /*func Scan2() string {
+	in := bufio.NewReader(os.Stdin)
+	str, err := in.ReadString('\n')
+	if err != nil {
+	  fmt.Println("Ошибка ввода: ", err)
+	}
+	return str
+  }*/
 
-func readInput() []string {
+/*func readInput() []string {
 	var str []string
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
@@ -20,4 +50,4 @@ func readInput() []string {
 		str = append(str, word)
 	}
 	return str
-}
+}*/
